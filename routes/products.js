e5ce4d7415f7c099c26dd97a2f55646319bcd0f1
susandv26 <valladares.susan@yahoo.com>
2024-12-14 04:51:17 -0600

    cambios en la rutas

--- routes/products.js
+++ routes/products.js
@@ -5,6 +5,6 @@ const productController = require('../controllers/productController');
 // Rutas para los productos obtener, agregar y  elimminar
 router.get('/', productController.getAllProducts);
 router.post('/', productController.createProduct);
-router.delet('/:id', productController.deleteProduct);
+router.delete('/:id', productController.deleteProduct);
 
 module.exports = router;
