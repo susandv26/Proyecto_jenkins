3b25b47586b34ec5ecef9f704783dd40e2e91925
susandv26 <valladares.susan@yahoo.com>
2024-12-16 15:54:51 -0600

    cambios buenos

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = 000;
+const PORT =4000;
 
 // Middleware
 app.use(bodyParser.json());
